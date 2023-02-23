977bf6aba5511afea6d3ae40a51df3ecc66247f1
mateoroldos <mateoroldos19@gmail.com>
2023-02-23 18:57:20 -0300

    feat: Header styling

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
+	import { page } from "$app/stores";
+
+console.log($page);
+
 
 </script>
 
 <header>
-  <h1>Piggy</h1>
-  <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-  </nav>
+  <div>
+    <a href="/" class="logo-link">
+      <h1>🐽 Piggy</h1>
+    </a>
+    <nav>
+      <a class="header-link" class:active={$page.url.pathname === "/"} href="/">Create</a>
+      <a class="header-link" class:active={$page.url.pathname === "/trending"} href="/trending">Trending</a>
+      <a class="header-link" class:active={$page.url.pathname === "/my-piggys"} href="/my-piggys">My Piggys</a>
+    </nav>
+  </div>
+  <div>
+    <a class="header-link" href="/login">Login</a>
+    <a class="button" href="/register">Register</a>
+  </div>
 </header>
 
-<style types="scss">
+<style lang="scss">
   header {
     position: fixed;
     display: flex;
@@ -18,5 +31,25 @@
     align-items: center;
     padding: 1rem;
     width: 100%;
+
+    .logo-link {
+      text-decoration: none;
+      color: var(--md-sys-color-on-background);      
+    }
+
+    nav {
+      margin-left: 5rem;
+      gap: 2.8rem;
+    }
+
+    > div, nav {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+
+    div:last-child {
+      gap: 1.2rem;
+    }
   }
 </style>
