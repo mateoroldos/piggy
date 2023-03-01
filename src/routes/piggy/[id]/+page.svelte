a69d7ff1947f479e2159443c50af6bbda58e3ef7
mateoroldos <mateoroldos19@gmail.com>
2023-02-28 23:15:46 -0300

    feat: Styling improvements

--- src/routes/piggy/[id]/+page.svelte
+++ src/routes/piggy/[id]/+page.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="main-wrapper">
-    <h2 class="display-small">{`${data.piggy.name} needs $FLOW ${data.piggy.goal.toLocaleString()} ${data.piggy.description}`}</h2>
+    <h2 class="display-small">{`${data.piggy.name} needs `}<span>{`$FLOW ${data.piggy.goal.toLocaleString()}`}</span>{` to ${data.piggy.description}`}</h2>
     <a class="button" href={`${$page.url.pathname}/donate`}>Donate</a>
 </div>
 
@@ -16,6 +16,16 @@
         flex-direction: column;
         align-items: flex-start;
         gap: 1rem;
+
+        h2 {
+            color: var(--md-sys-color-on-background);
+
+             span {
+                color: var(--md-sys-color-primary);
+            }
+        }
+
+       
 
         .button {
             font-size: 1.2rem;
