19c091b287f020fa9267218251dc15350378492c
mateoroldos <mateoroldos19@gmail.com>
2023-03-01 04:31:21 -0300

    feat: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div>
+    <h1>{$page.status}: {$page.error?.message}</h1>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+</style>
